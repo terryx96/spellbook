59978b36293f17c21f1d1c2c98a0a7f2b85e0a6c
Terry Wade <terry.wade@valpo.edu>
2018-06-04 16:35:35 -0400

    Added Button to change text (HW 1)

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const button = document.querySelector('button')
 
 function changeText(){
-        
+    const header = document.querySelector("h1");
+    header.textContent = "Not Spellbook";
 }
+
+button.addEventListener("click", changeText);
