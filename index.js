e271d269960e1845207289a29d88c1c40a2c3e3d
Terry Wade <terry.wade@valpo.edu>
2018-06-04 17:32:57 -0400

    Added the Enter Key Extra Credit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const button = document.querySelector('#change')
 const submitButton = document.querySelector("#submitButton");
+const inputField = document.querySelector("input");
 
 function changeText(){
     const header = document.querySelectorAll("h1");
@@ -10,7 +11,14 @@ function submit(){
     const thirdHeader = document.querySelectorAll("h1")[2];
     const typedText = document.querySelector("input").value;
     thirdHeader.textContent = typedText;
+
 }
 
 button.addEventListener("click", changeText);
 submitButton.addEventListener("click", submit);
+
+inputField.addEventListener("keyup", function(event){
+    if(event.keyCode === 13){
+        document.querySelectorAll("h1")[2].textContent = document.querySelector("input").value;
+    }
+})
