ceeaf206d125a192c89f92c7d9debda2452eb045
Terry Wade <terry.wade@valpo.edu>
2018-06-04 17:24:39 -0400

    Added two Extra Credit Assignments

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const button = document.querySelector('button')
+const button = document.querySelector('#change')
+const submitButton = document.querySelector("#submitButton");
 
 function changeText(){
-    const header = document.querySelector("h1");
-    header.textContent = "Not Spellbook";
+    const header = document.querySelectorAll("h1");
+    header[1].textContent = "Not Spellbook";
+}
+
+function submit(){
+    const thirdHeader = document.querySelectorAll("h1")[2];
+    const typedText = document.querySelector("input").value;
+    thirdHeader.textContent = typedText;
 }
 
 button.addEventListener("click", changeText);
+submitButton.addEventListener("click", submit);
