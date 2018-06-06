faf9aa7a918bbaa741a09a5309f903ed084a091a
Terry Wade <terry.wade@valpo.edu>
2018-06-06 09:38:11 -0400

    Enhance the spell input with a level and new CSS

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const form = document.querySelector('form')
 const desc = document.querySelector("#description")
+const level = document.querySelector("#level")
 
 const changeHeading = function(ev) {
-  ev.preventDefault()
-
-  const f = ev.target
-  const spellName = f.spellName.value;
-  const spellDesc = desc.value;
+  ev.preventDefault();
+  const f = ev.target;
   const spellsDiv = document.querySelector('#spells');
-  const node = createLi(spellName, spellDesc);
+  const node = createLi(f.spellName.value, f.spellDescription.value, f.level.value);
   spellsDiv.appendChild(node);
   f.reset()
 }
 
-function createLi(spellName, spellDesc){
+function createLi(spellName, spellDesc, level){
     const node = document.createElement("LI");
-    node.appendChild(createSpan(spellName, "name"));
-    node.appendChild(createSpan(spellDesc, "desc"));
+    node.appendChild(createSpan(`${spellName}: `, "name"));
+    node.appendChild(createSpan(`${spellDesc}`, "desc"));
+    node.appendChild(createSpan(`   Level ${level}`, determineLevel(level)));
     return node;
 }
 
@@ -25,6 +24,18 @@ function createSpan(value, id){
     span.id = id;
     span.textContent = value;
     return span;
+}
+
+function determineLevel(level){
+    let id = "";
+    switch(level){
+        case "0": id = "brown"; break;
+        case "1": id = "purple"; break;
+        case "2": id = "blue"; break;
+        case "3": id = "yellow"; break;
+        default: id = "rainbow"; break;
+    }
+    return id;
 }
 
 form.addEventListener('submit', changeHeading);
