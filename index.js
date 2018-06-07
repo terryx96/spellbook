d621fac741f528712973fff0d2d8fb0b9022c38a
Terry Wade <terry.wade@valpo.edu>
2018-06-06 23:12:56 -0400

    Store each spell as an object in an array

--- index.js
+++ index.js
@@ -1,41 +1,77 @@
-const form = document.querySelector('form')
-const desc = document.querySelector("#description")
-const level = document.querySelector("#level")
 
-const changeHeading = function(ev) {
-  ev.preventDefault();
-  const f = ev.target;
-  const spellsDiv = document.querySelector('#spells');
-  const node = createLi(f.spellName.value, f.spellDescription.value, f.level.value);
-  spellsDiv.appendChild(node);
-  f.reset()
-}
 
-function createLi(spellName, spellDesc, level){
-    const node = document.createElement("LI");
-    node.appendChild(createSpan(`${spellName}: `, "name"));
-    node.appendChild(createSpan(`${spellDesc}`, "desc"));
-    node.appendChild(createSpan(`   Level ${level}`, determineLevel(level)));
-    return node;
-}
+const app = {
+    spellArr: [],
+    
+    init: function(){
+        const form = document.querySelector('form')
+        form.addEventListener('submit', (ev) => {
+            ev.preventDefault();
+            this.addSpell(ev);
+            console.log(this.spellArr);
+        })},
 
-function createSpan(value, id){
-    const span = document.createElement("span");
-    span.id = id;
-    span.textContent = value;
-    return span;
-}
+    addSpell: function(ev) {
+        ev.preventDefault();
+        const f = ev.target;
+        const spellsDiv = document.querySelector('#spells');
+        const node = this.createLi(f.spellName.value, f.spellDescription.value, f.level.value);
+        const spell = {
+            name: f.spellName.value,
+            description: f.spellDescription.value,
+            spellLevel: f.level.value,
+        }
+        this.spellArr.push(spell);
+        spellsDiv.appendChild(node);
+        f.reset()
+      },
 
-function determineLevel(level){
-    let id = "";
-    switch(level){
-        case "0": id = "brown"; break;
-        case "1": id = "purple"; break;
-        case "2": id = "blue"; break;
-        case "3": id = "yellow"; break;
-        default: id = "rainbow"; break;
+    createLi: function(spellName, spellDesc, level){
+        const node = document.createElement("li");
+        node.classList.add("inline")
+        node.appendChild(this.createSpan(`${spellName}: `, "name"));
+        node.appendChild(this.createSpan(`${spellDesc}`, "desc"));
+        node.appendChild(this.createSpan(`Level ${level}`, this.determineLevel(level)));
+        node.appendChild(this.deleteButton());
+        return node;
+    },
+
+    createSpan: function(value, id){
+        const span = document.createElement("span");
+        span.setAttribute("id", id);
+        span.textContent = value;
+        return span;
+    },
+
+    determineLevel: function(level){
+        let id = "";
+        switch(level){
+            case "0": id = "brown"; break;
+            case "1": id = "purple"; break;
+            case "2": id = "blue"; break;
+            case "3": id = "yellow"; break;
+            default: id = "rainbow"; break;
+        }
+        return id;
+    },
+
+    deleteButton: () => {
+        const del = document.createElement("input");
+        del.setAttribute("type", "submit");
+        del.setAttribute("value", "Delete");
+        del.onclick = function(){
+           del.parentNode.parentNode.removeChild(del.parentNode)
+           app.spellArr.splice(app.spellArr.indexOf(del.parentNode), 1);
+            
+        }
+        return del;
     }
-    return id;
 }
 
-form.addEventListener('submit', changeHeading);
+
+
+app.init();
+
+
+
+
